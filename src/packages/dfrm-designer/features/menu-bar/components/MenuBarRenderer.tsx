cca0476ca872a52bcf1f480820cd46d6729ae58a
Gabor Sar <gabor.sar@gmail.com>
2025-02-24 10:00:20 +0100

    removed unused imports

--- src/packages/dfrm-designer/features/menu-bar/components/MenuBarRenderer.tsx
+++ src/packages/dfrm-designer/features/menu-bar/components/MenuBarRenderer.tsx
@@ -1,23 +1,11 @@
 import React from "react";
 import {
-  VscChromeClose,
-  VscChromeMaximize,
-  VscChromeMinimize,
-  VscChromeRestore,
-} from "react-icons/vsc";
-import {
   WorkbenchMenuBar,
   WorkbenchMenuBarCenter,
   WorkbenchMenuBarLeft,
   WorkbenchMenuBarRight,
 } from "../../../components/Workbench";
 import { useEditStackItem } from "../../../model";
-import {
-  useCloseWindowTrigger,
-  useIsWindowMaximized,
-  useMaximizeWindowTrigger,
-  useMinimizeWindowTrigger,
-} from "../../electron";
 import { LanguageMenu } from "./LanguageMenu";
 import { MainMenu } from "./MainMenu";
 import { ToggleBottomPanelButton } from "./ToggleBottomPanelButton";
